9f9cfa40be03c59c69a1c5a84fad4f112b15541f
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2021-06-15 15:55:27 +0200

    Helpers

---
+++ background/bih_cluster_job.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Set a name for the job (-J or --job-name).
+#SBATCH --job-name=enrichment
+
+# Set the file to write the stdout and stderr to (if -e is not set; -o or --output).
+#SBATCH --output=logs/%x-%j.log
+
+# Set the number of cores (-n or --ntasks).
+#SBATCH --ntasks=2
+
+# Force allocation of the two cores on ONE node.
+#SBATCH --nodes=1
+
+# Set the total memory. Units can be given in T|G|M|K.
+#SBATCH --mem=1G
+
+# Optionally, set the partition to be used (-p or --partition).
+#SBATCH --partition=critical
+
+# Set the expected running time of your job (-t or --time).
+# Formats are MM:SS, HH:MM:SS, Days-HH, Days-HH:MM, Days-HH:MM:SS
+#SBATCH --time=7-00:00
+
+
+export TMPDIR=/fast/users/${USER}/scratch/tmp
+export LOGDIR=logs/${SLURM_JOB_NAME}-${SLURM_JOB_ID}
+mkdir -p $LOGDIR
+
+unset DRMAA_LIBRARY_PATH
+eval "$($(which conda) shell.bash hook)"
+conda activate first-steps
+
+set -x
+
+JOBS=100
+
+# Note that Slurm DRMAA differs slightly from original Slurm syntax
+# --mem-per-cpu doesn't accept units and the default unit here is MB
+# -t only accepts HH:MM
+snakemake \
+    --cluster-config bih_cluster_config.json \
+    --drmaa " \
+        -p critical \
+        -t {cluster.time} \
+        --nodes=1 \
+        --mem={cluster.memory} \
+        -n {cluster.tasks} \
+        -o $LOGDIR/%x-%j.log" \
+    -j $JOBS \
+    -k \
+    -p
+
